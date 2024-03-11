5ce59db3eb35d6cb3a4eb80df0956082db6168e7
Juan Sebastian Giraldo <juans.giraldo.e@gmail.com>
2024-03-11 16:32:36 -0500

    [Update] - Make downloadable. // Cache files.

--- sw.js
+++ sw.js
@@ -1,11 +1,33 @@
 //Cuando se instala el service worler.
+const nombreCache = 'apv-v1';
+const archivos = [
+  '/',
+  '/index.html',
+  '/css/bootstrap.css',
+  '/css/styles.css',
+  'js/app.js',
+  'js/apv.js',
+];
+
+
 self.addEventListener('install', e => {
   console.log('Instalando...');
-  console.log(e);
+  e.waitUntil(
+    caches.open(nombreCache)
+      .then( cache => {
+        cache.addAll(archivos);
+      })
+  );
+
 });
 
 //Cuando se activa el service worker.
 self.addEventListener('activate', e => {
   console.log('Activado');
   console.log(e);
+});
+
+//Evento fetch para descargar.
+self.addEventListener('fetch', e => {
+  console.log('Fetch...', e);
 });
